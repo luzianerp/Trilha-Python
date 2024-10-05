b0937d73a85c1c921995f6dfc322cc0f19bf41f6
luzianerp <95301530+luzianerp@users.noreply.github.com>
2024-10-04 21:34:54 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,34 @@
+print('hello word') #string
+...
+Aula 2 04/10/2004
+...
+# print('morango','  banana') #string
+# print(123) #1 int
+# print(37438,23534) #2 int
+# print(2963.44) #float
+# print(True) #boolean
+# print(False) #boolean
+# print(None) #none
+# print(['string']) #listas
+# print(('string')) #tupla
+#  print({'string':'texto'}) #dicionário
+# operador lógico (conectivos de condições) => and (E), or(OU), not(Não), !=(diferente), <=, >=, == (igual é comparação)
+
+
+# temperatura1=36 #Cada VARIÁVEL armazena apenas 1 INFORMAÇÃO por vez. Se o nome da variável for o mesmo, o Python irá SOBRESCREVER essa informação.
+# temperatura2=37.5 #Por isso cada temperatura ao lado está com um nome diferente.
+# temperatura3=39.6 
+# print (temperatura1,temperatura2,temperatura3)
+
+# if temperatura1<37.5: #Nosso primeiro teste lógico. Sempre termino com ':'
+#     print("Você não está com febre.") #Aqui temos uma regra de IDENTAÇÃO: toda resposta à minha condição deve estar DENTRO do meu if (lembrem-se da tecla TAB para corrigir a identação)
+# elif temperatura1==37.5: #Nosso segundo teste lógico. Sempre termino com ':'
+#     print("Você está febril.")
+# else: #Última opção condicional. É requisitada quando todas as condições anteriores deram FALSE. Haverão momentos em que o 'else' não precisará ser escrito (quando eu não desejo dar resposta ao 'else')
+#     print("Você está febre.")
+
+idade= num.
+
+if idade<12:
+    print(' menor de idade ')
+    else
