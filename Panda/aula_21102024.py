34c653483ef7d74c4459024eeee7b0ce43498ab4
luzianerp <95301530+luzianerp@users.noreply.github.com>
2024-10-21 20:56:18 -0300

    Add files via upload
    Abrir arquivo em cvs

---
+++ Panda/aula_21102024.py
@@ -0,0 +1,5 @@
+import pandas as pd
+df_livros = pd.read_csv('books_of_the_decade.csv')
+print(df_livros.head())  # Exibe as primeiras linhas
+print(df_livros.tail())  # Exibe as cinco últimas linhas
+
