0b995dc8547b0cf9fe0488c751339694deb0b24f
luzianerp <95301530+luzianerp@users.noreply.github.com>
2024-10-24 18:37:16 -0300

    Add files via upload
    Banco de dados => Arquivo Python and SQL

---
+++ UC2/SQL/Atividade01.sql
@@ -0,0 +1,5 @@
+-- consultando as injeções realizadas
+SELECT * FROM alunos WHERE matricula=1
+
+--ATIVIDADE: Crie uma nova tabela chamada 'professores', com a mesma quantidade de características de 'alunos',
+--fazendo ao menos duas injeções de dados e uma consulta.
