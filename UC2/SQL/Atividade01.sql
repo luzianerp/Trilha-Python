3fc14110daa517f5c068a26eb72f5eb0564eb8a2
luzianerp <95301530+luzianerp@users.noreply.github.com>
2024-10-24 18:36:19 -0300

    Delete UC2 directory

--- UC2/SQL/Atividade01.sql
+++
@@ -1,5 +0,0 @@
--- consultando as injeções realizadas
-SELECT * FROM alunos WHERE matricula=1
-
---ATIVIDADE: Crie uma nova tabela chamada 'professores', com a mesma quantidade de características de 'alunos',
---fazendo ao menos duas injeções de dados e uma consulta.
